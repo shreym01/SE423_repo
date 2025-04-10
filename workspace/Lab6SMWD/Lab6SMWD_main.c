b7d7bc653b4cb524cf002daa089f3ed0c32bc59f
Deng <wd10@illinois.edu>
2025-04-10 14:37:22 -0500

    update v2

--- workspace/Lab6SMWD/Lab6SMWD_main.c
+++ workspace/Lab6SMWD/Lab6SMWD_main.c
@@ -1263,7 +1263,7 @@ void setupSpib(void) //Call this function in main() somewhere after the DINT; li
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <sys/ioctl.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
